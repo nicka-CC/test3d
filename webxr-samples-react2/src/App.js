6578edd2127aca5b57ba2b9841080d078b5e761a
nikita <im.nicka.cc@gmail.com>
2025-05-28 02:15:49 +0300

    nо

--- webxr-samples-react2/src/App.js
+++ webxr-samples-react2/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import * as THREE from "three";
-import { useThree, useFrame } from "@react-three/fiber";
+import { useThree, useFrame, Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { ARCanvas } from "@react-three/xr";
+import { ARButton, XR, Controllers, Hands } from "@react-three/xr";
 
 import { DefaultCube } from "./DefaultCube";
 
 export default function App() {
     return (
-        <ARCanvas camera={{ position: [0, 1.5, 4] }}>
-            <ambientLight intensity={0.5} />
-            <pointLight position={[10, 10, 10]} />
-            <DefaultCube />
-        </ARCanvas>
+        <>
+            <ARButton />
+            <XR>
+                <Canvas camera={{ position: [0, 1.5, 4] }}>
+                    <ambientLight intensity={0.5} />
+                    <pointLight position={[10, 10, 10]} />
+                    <Controllers />
+                    <Hands />
+                    <DefaultCube />
+                </Canvas>
+            </XR>
+        </>
     );
 }
